0b7e5b9c0f23b14b9c10019f381e7f26d453416b
Pudlo Anton <pudlo.anton@gmail.com>
2016-07-13 21:48:45 +0300

    Question click change question 2

--- GeoQuiz Challenge/app/src/main/java/com/example/tmp/geoquiz/QuizActivity.java
+++ GeoQuiz Challenge/app/src/main/java/com/example/tmp/geoquiz/QuizActivity.java
@@ -84,7 +84,7 @@ public class QuizActivity extends AppCompatActivity {
 
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
             }
