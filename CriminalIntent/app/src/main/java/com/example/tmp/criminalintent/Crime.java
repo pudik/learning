59705129858ecef7328aac6f206cf6fef42aed11
Pudlo Anton <pudlo.anton@gmail.com>
2016-08-02 21:46:44 +0300

    Added buttons and landscape mod

--- CriminalIntent/app/src/main/java/com/example/tmp/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/example/tmp/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.tmp.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,28 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 
     public UUID getId() {
--- CriminalIntent/app/src/main/java/com/example/tmp/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/example/tmp/criminalintent/CrimeActivity.java
@@ -1,21 +1,28 @@
 package com.example.tmp.criminalintent;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentManager;
+
 
 public class CrimeActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.content_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+
     }
 
 
