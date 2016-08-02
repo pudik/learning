135e878663d835ae2f4a64f4092475e77a638209
Pudlo Anton <pudlo.anton@gmail.com>
2016-08-02 22:21:19 +0300

    Date on Button beautify

--- CriminalIntent/app/src/main/java/com/example/tmp/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/example/tmp/criminalintent/CrimeFragment.java
@@ -50,7 +50,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setText(android.text.format.DateFormat.format("EEEE, MMM d, yyyy",mCrime.getDate()).toString());
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crive_solved);
