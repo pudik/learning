9b527ce38245bcff47deb5f4642807c511c444a2
Pudlo Anton <pudlo.anton@gmail.com>
2016-09-06 21:02:49 +0300

    Added suspect and crime report

--- CriminalIntent/app/src/main/java/com/example/tmp/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/com/example/tmp/criminalintent/Crime.java
@@ -12,6 +12,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime(){
         this(UUID.randomUUID());
@@ -36,6 +37,14 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
+    }
+
+    public String getSuspect() {
+        return mSuspect;
+    }
+
+    public void setSuspect(String suspect) {
+        mSuspect = suspect;
     }
 
     public UUID getId() {
